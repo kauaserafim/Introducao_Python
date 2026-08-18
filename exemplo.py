700cf27bc4a52cbca84d1bbdfd74b3219e58be5b
KAUA SANTOS SERAFIM DE OLIVEIRA <kauaoliveira@adm.unitoledo.br>
2026-08-17 21:43:34 -0300

    primeiro commit

---
+++ exemplo.py
@@ -0,0 +1,41 @@
+# Inserindo uma informação
+print("Meu nome é Kauã")
+
+# Identificando um Tipo de Dado
+print(type(10))
+print(type("olá mundo"))
+print(type(3.5))
+print(type(3 + 2j))
+
+# Manipulando Variáveis
+nome = "Kauã"
+print(nome)
+
+idade = 19
+print(idade)
+print(type(idade))
+
+# Concatenendo Variáveis
+
+nome = "Bruna"
+sobrenome = "Serafim"
+nome_completo = nome + " " + sobrenome
+print(nome_completo)
+
+# Multiplicar Strings
+cidade = 'Presidente Prudente ' * 3
+print(cidade)
+
+# Funções Nativas para Strings
+mensagem = "O palmeiras não tem mundial!"
+print(mensagem.upper()) # Maiúsculo
+print(mensagem.lower()) # Minúsculo
+print(mensagem.capitalize()) # Primeira Letra de cada palavra Maiúsculo
+print(mensagem.isnumeric()) # Identificar se é Número
+print(mensagem.replace(" não", "")) # Substitui a palavra por outra
+
+# Dados do Teclado
+idade = input("informe sua idade: ")
+print("sua idade eh: ",idade)
+
+
